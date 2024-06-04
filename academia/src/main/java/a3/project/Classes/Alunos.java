195d2fb6a94ce30cfb519c8b58fc82f4aa82dcc6
fstngod <victordfaustino@gmail.com>
2024-06-04 20:49:01 -0300

    Create Alunos.java

---
+++ academia/src/main/java/a3/project/Classes/Alunos.java
@@ -0,0 +1,30 @@
+package classes;
+
+public class Alunos extends Pessoas {
+    private double peso;
+    private double altura;
+    public void setPeso (double peso){
+        this.peso = peso;
+    }
+    public double getPeso(){
+        return peso;
+    }
+    
+    public void setAltura (double altura){
+        this.altura = altura;
+    }
+    public double getAltura(){
+        return altura;
+    }
+@Override
+public String toString(){
+  return "Aluno" + "\n" +
+          "Nome:" + nome+ "\n" +
+          "DataDeNascimento:" + DataDeNascimento+ "\n" +
+          "Cpf:" + cpf+"\n" +
+          "Senha:" + senha+"\n" +
+          "peso:" + peso+"\n" +
+          "altura:" + altura+"\n" ;       
+    }
+
+}
